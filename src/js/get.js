93dba07a8331e81f8d91d4ab298ea78384ed1114
Olexiy-repin <31596886+Olexiy-repin@users.noreply.github.com>
2022-06-06 12:23:06 +0300

    classwork

---
+++ src/js/get.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+ * Method (GET).
+ */
+
+// const BASE_URL = 'http://localhost:3000';
+
+// fetch(`${BASE_URL}/users/5`, {
+//   method: 'GET',
+// })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
---
+++ src/js/put-patch.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/*
+ * Method(PUT/PATCH). Headers ("Content-Type": "application/json"). Body.
+ */
+
+const BASE_URL = 'http://localhost:3000';
+
+// const updateUserById = (userId, newData) => {
+//   return fetch(`${BASE_URL}/users/${userId}`, {
+//     method: 'PUT',
+//     body: JSON.stringify(newData),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+// };
+
+// updateUserById(15, {
+//   name: 'Mark Davis',
+//   email: 'cektet@tajuh.ug',
+// })
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
